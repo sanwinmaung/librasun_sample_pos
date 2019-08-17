bfa27e83c7dd2e00293d7bda36fcf6b5ce764567
San Win Maung <sanwinmaung.dev@gmail.com>
2019-08-18 02:44:51 +0630

    Just Update

--- resources/views/layouts/nav-top.blade.php
+++ resources/views/layouts/nav-top.blade.php
@@ -21,10 +21,10 @@
       </ul>
       <ul class="navbar-nav navbar-nav-right">
         <li class="nav-item dropdown">
-          <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
+          {{-- <a class="nav-link count-indicator dropdown-toggle" id="notificationDropdown" href="#" data-toggle="dropdown">
             <i class="mdi mdi-bell-outline mx-0"></i>
             <span class="count"></span>
-          </a>
+          </a> --}}
           <div class="dropdown-menu dropdown-menu-right navbar-dropdown preview-list" aria-labelledby="notificationDropdown">
             <a class="dropdown-item">
               <p class="mb-0 font-weight-normal float-left">You have 4 new notifications
